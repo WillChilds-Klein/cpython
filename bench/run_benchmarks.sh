2b408b7cd2264b8c4d3cf20c07362e8c913b9960
WillChilds-Klein <willck93@gmail.com>
2024-01-05 20:55:41 +0000

    Run benchmark against OpenSSL master + 1.1.1

--- bench/run_benchmarks.sh
+++ bench/run_benchmarks.sh
@@ -5,42 +5,85 @@ set -exuo pipefail
 
 BENCH_DIR=$(realpath ./bench)
 SCRATCH_DIR=$BENCH_DIR/scratch
-mkdir -p $SCRATCH_DIR
+REPORT_FILE=$BENCH_DIR/report.txt
 
-function setup_vars() {
-    SCRATCH_DIR_OSSL=$SCRATCH_DIR/${1}
+function setup() {
+    local repo=${1}
+    local branch=${2}
+    local dir_name=$(echo -n $repo | cut -d/ -f2)-${branch}
+
+    # these are intentionally global
+    SCRATCH_DIR_OSSL=$SCRATCH_DIR/${dir_name}
     SRC_DIR=$SCRATCH_DIR_OSSL/src
     INSTALL_DIR=$SCRATCH_DIR_OSSL/install
-    BUILD_DIR=$SCRATCH_DIR_OSSL/build
-    mkdir -p $SCRATCH_DIR_OSSL $SRC_DIR $INSTALL_DIR $BUILD_DIR
+    mkdir -p $SCRATCH_DIR_OSSL $SRC_DIR $INSTALL_DIR
+
+    # this assumes all sources managed under git
+    [[ -d ${SRC_DIR}/.git ]] \
+        || git clone \
+            --depth 1 \
+            --branch ${branch} \
+            https://github.com/${repo}.git \
+            $SRC_DIR
 }
 
-setup_vars aws-lc
+function build_awslc() {
+    pushd $SRC_DIR
+    local fips=${1:-"OFF"}
+    local build_dir=./build
+    cmake \
+        -B${build_dir} \
+        -S${SRC_DIR} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_PREFIX_PATH=${INSTALL_DIR} \
+        -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
+        -DFIPS=$fips \
+        -DBUILD_LIBSSL=ON \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DBUILD_TESTING=OFF
+    make -C ${build_dir} -j $(nproc) install
+    popd
+}
 
-[[ -f ${SRC_DIR}/README.md ]] \
-    || git clone \
-        --branch libssl-handle-EAGAIN \
-        https://github.com/WillChilds-Klein/aws-lc.git \
-        $SRC_DIR
+function build_openssl() {
+    pushd ${SRC_DIR}
+    ./config \
+        --prefix=${INSTALL_DIR} \
+        --openssldir=${INSTALL_DIR} \
+        -d
+    make -j$(nproc)
+    make install_sw
+    # some systems install under "lib64" instead of "lib", so ensure both
+    [[ -d ${INSTALL_DIR}/lib64 ]] || ln -s ${INSTALL_DIR}/lib{,64}
+    [[ -d ${INSTALL_DIR}/lib   ]] || ln -s ${INSTALL_DIR}/lib{64,}
+    popd
+}
 
-cmake \
-    -B${BUILD_DIR} \
-    -S${SRC_DIR} \
-    -DCMAKE_BUILD_TYPE=Release\
-    -DCMAKE_PREFIX_PATH=${INSTALL_DIR} \
-    -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
-    -DBUILD_LIBSSL=ON \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DBUILD_TESTING=OFF
-make -C ${BUILD_DIR} -j $(nproc) install
+function build_python() {
+    sed -i -e "s|^OPENSSL=.*$|OPENSSL=${INSTALL_DIR}|g" ./Modules/Setup
+    ./configure \
+        --with-openssl=${INSTALL_DIR} \
+        --with-builtin-hashlib-hashes=blake2 \
+        --with-ssl-default-suites=openssl \
+        --prefix=${INSTALL_DIR}
+    make -j$(nproc)
+}
 
+function main() {
+    setup WillChilds-Klein/aws-lc libssl-handle-EAGAIN
+    build_awslc
+    build_python
+    ./python $BENCH_DIR/benchmarks.py | tee $REPORT_FILE
 
-sed -i -e "s|^OPENSSL=.*$|OPENSSL=${INSTALL_DIR}|g" ./Modules/Setup
-./configure \
-    --with-openssl=${INSTALL_DIR} \
-    --with-builtin-hashlib-hashes=blake2 \
-    --with-ssl-default-suites=openssl \
-    --prefix=${INSTALL_DIR}
-make -j$(nproc)
+    setup openssl/openssl master
+    build_openssl
+    build_python
+    ./python $BENCH_DIR/benchmarks.py |& tee $REPORT_FILE
 
-./python $BENCH_DIR/benchmarks.py
+    setup openssl/openssl OpenSSL_1_1_1-stable
+    build_openssl
+    build_python
+    ./python $BENCH_DIR/benchmarks.py |& tee $REPORT_FILE
+}
+
+main
