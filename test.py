322fd9f58314d7bb72305aab73adeffc90eaf007
WillChilds-Klein <willck93@gmail.com>
2024-07-31 16:45:43 +0000

    Use secretsmanager endpoint in test script

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import ssl
 import socket
 
+assert "AWS-LC" in ssl.OPENSSL_VERSION
+
 ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 ctx.set_ecdh_curve("X25519Kyber768Draft00")
 ctx.load_verify_locations("/etc/ssl/certs/ca-certificates.crt")
 
-host = "kms.us-east-1.amazonaws.com"
+host = "secretsmanager.us-east-1.amazonaws.com"
 sock = socket.create_connection((host, 443))
 ssock = ctx.wrap_socket(sock, server_hostname=host)
 ssock.close()
